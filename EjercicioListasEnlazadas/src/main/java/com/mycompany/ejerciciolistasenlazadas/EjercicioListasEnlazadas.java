fffeeed7f1ccc9d746f67719f71e2e0f12062460
David Castillo <harold.castillo.r@outlook.es>
2024-02-23 09:12:49 -0500

    Adicion de ejercicio de listas enlazadas simples

---
+++ EjercicioListasEnlazadas/src/main/java/com/mycompany/ejerciciolistasenlazadas/EjercicioListasEnlazadas.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciolistasenlazadas;
+
+/**
+ *
+ * @author hdavidcastillo
+ */
+public class EjercicioListasEnlazadas {
+
+    public static void main(String[] args) {
+        ListaEnlazada lista = new ListaEnlazada();
+              
+        // Agregar al final de la lista
+        lista.agregarAlFinal(12);
+        lista.agregarAlFinal(15);
+        lista.agregarAlFinal(9);
+        
+        System.out.println("Ejemplo de lista Simple\nImpresion de lista antes de agregar");
+        lista.listar();
+        lista.agregarAlFinal(8);
+        System.out.println("\nImpresion de lista despues de agregar");
+        lista.listar();
+        
+        System.out.println("\nImpresion de lista antes de eliminar - Eliminando posicion 1");
+        lista.listar();
+        lista.removerPorPosicion(1);
+        System.out.println("\nImpresion de lista despues de eliminar");
+        lista.listar();
+        
+        System.out.println("\nImpresion de lista");
+        lista.listar();
+    }
+}
