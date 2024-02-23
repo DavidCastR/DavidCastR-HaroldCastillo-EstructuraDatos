9d0018f853b61f7f375a10f02d12de1ba59094fb
David Castillo <harold.castillo.r@outlook.es>
2024-02-23 09:54:44 -0500

    Correcion con problema del POM no permitia ejecutar y se agregan saltos de linea para mejor legilibidad

--- EjercicioListasEnlazadas/src/main/java/com/mycompany/ejerciciolistasenlazadas/EjercicioListasEnlazadas.java
+++ EjercicioListasEnlazadas/src/main/java/com/mycompany/ejerciciolistasenlazadas/EjercicioListasEnlazadas.java
@@ -18,19 +18,19 @@ public class EjercicioListasEnlazadas {
         lista.agregarAlFinal(15);
         lista.agregarAlFinal(9);
         
-        System.out.println("Ejemplo de lista Simple\nImpresion de lista antes de agregar");
+        System.out.println("Ejemplo de lista Simple\n\nImpresion de lista antes de agregar");
         lista.listar();
         lista.agregarAlFinal(8);
         System.out.println("\nImpresion de lista despues de agregar");
         lista.listar();
         
-        System.out.println("\nImpresion de lista antes de eliminar - Eliminando posicion 1");
+        System.out.println("\n\nImpresion de lista antes de eliminar - Eliminando posicion 1");
         lista.listar();
         lista.removerPorPosicion(1);
         System.out.println("\nImpresion de lista despues de eliminar");
         lista.listar();
         
-        System.out.println("\nImpresion de lista");
+        System.out.println("\n\nImpresion de lista");
         lista.listar();
     }
 }
